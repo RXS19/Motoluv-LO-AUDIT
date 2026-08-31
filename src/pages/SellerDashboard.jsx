449d033da9acf85889778d9e4481d331d2c30251
RXS19 <rsalinasn11@gmail.com>
2026-08-31 11:11:43 -0600

    feat(SellerDashboard): import useMemo hook

--- src/pages/SellerDashboard.jsx
+++ src/pages/SellerDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import {
   Bike,
